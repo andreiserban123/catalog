09fcb869aed305497b8edc7887da2cc8e7a8a834
Andrei Serban <andyspeed2003@gmail.com>
2023-04-12 23:10:33 +0300

    react cleanup

--- routes/catalogue.js
+++ routes/catalogue.js
@@ -7,6 +7,7 @@ router.get('/juds', async (req, res) => {
   try {
     const connection = await oracledb.getConnection()
     const result = await connection.execute(`SELECT * FROM SIT_JUD`)
+    connection.close()
     res.json({ succes: true, data: result.rows })
   } catch (err) {
     console.error(err.message)
@@ -24,7 +25,40 @@ router.get('/juds/:id', async (req, res) => {
         id,
       }
     )
+    connection.close()
+
     res.json({ succes: true, data: result.rows })
+  } catch (err) {
+    console.error(err.message)
+    throw err
+  }
+})
+
+router.get('/user', async (req, res) => {
+  try {
+    const connection = await oracledb.getConnection()
+    const result = await connection.execute(`SELECT * FROM SIT_USER`)
+    connection.close()
+    res.json({ succes: true, data: result.rows })
+  } catch (err) {
+    console.error(err.message)
+    throw err
+  }
+})
+
+router.delete('/user/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const connection = await oracledb.getConnection()
+    const result = await connection.execute(
+      `DELETE FROM SIT_USER WHERE ID = :id`,
+      {
+        id,
+      }
+    )
+    connection.commit()
+    connection.close()
+    res.json({ succes: true, data: result })
   } catch (err) {
     console.error(err.message)
     throw err
@@ -37,11 +71,12 @@ router.post('/user', async (req, res) => {
     const { name, password, email } = req.body
     const result = await connection.execute(
       `INSERT INTO SIT_USER (id, NAME, password, email, id_pers)
-       VALUES (2, :name, :password, :email, 1) 
+        VALUES (SIT_USER_SQ.nextval, :name, SIT_UTILS_PKG.encrypt_password(:password), :email, 2) 
       `,
       { name, password, email }
     )
     connection.commit()
+    connection.close()
     res.json({ succes: true, data: result })
   } catch (err) {
     console.error(err.message)
