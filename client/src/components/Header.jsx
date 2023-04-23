3f9286eef56fc23c854e41af0c0012b2d3c4172f
Andrei Serban <andyspeed2003@gmail.com>
2023-04-23 16:05:22 +0300

    sdf

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,33 +1,41 @@
 import { Link } from 'react-router-dom';
-
+import { FaSchool, FaUserGraduate } from 'react-icons/fa';
+import { MdContactSupport } from 'react-icons/md';
+import { useState } from 'react';
 const Header = () => {
   return (
-    <nav className="h-screen w-20 border-2 bg-indigo-700  border-indigo-700 border-solid fixed left-0 rounded-br-3xl rounded-tr-3xl">
+    <nav className="h-screen w-7 md:w-20 border-2 bg-indigo-700  border-indigo-700 border-solid fixed left-0 rounded-br-3xl rounded-tr-3xl">
       <ul className="flex flex-col justify-start space-y-14 mt-5 h-full ">
         <li className="text-center text-xl font-bold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-300 ">
           <a
-            className="text-white text-center hover: border-b-neutral-200 border-1 border-solid "
+            className="text-white text-center hover: border-b-neutral-200 border-1 border-solid flex flex-col items-center "
             href="/"
           >
             <span className="">Home</span>
-            <i className="fa-solid fa-school"></i>
+            <FaSchool />
           </a>
         </li>
         <li className="text-center text-xl font-bold transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300 ">
-          <a className="text-white" href="/about">
+          <a
+            className="text-white flex flex-col justify-center items-center"
+            href="/about"
+          >
             <div>Profile</div>
-            <i className="fas fa-user"></i>
+            <FaUserGraduate />
           </a>
         </li>
         <li className="text-center text-xl font-bold transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300">
-          <a className="text-white " href="/contact">
+          <a
+            className="text-white flex flex-col items-center justify-center "
+            href="/contact"
+          >
             Contact
-            <i className="fa-solid fa-phone"></i>
+            <MdContactSupport />
           </a>
         </li>
-        <li className="text-center text-xl font-bold fixed bottom-10 left-2 duration-500 hover:-translate-y-6  w-6 h-6 ">
+        <li className="text-center text-xl font-bold fixed bottom-10 left-2 duration-500 hover:-translate-y-1  w-6 h-6 ">
           <Link to="/login" className="text-white">
-            <div>Logout</div>
+            <div className="sm:hidden">Logout</div>
             <i className="fa-solid fa-right-from-bracket"></i>
           </Link>
         </li>
