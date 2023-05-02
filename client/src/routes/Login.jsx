3dd5cd91ab2be29a46f705cf786fbb1f81420844
Andrei Serban <andyspeed2003@gmail.com>
2023-05-02 09:39:48 +0300

    f

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -19,14 +19,13 @@ const Login = () => {
         <div className="p-6 md:p-20">
           <h2 className="font-mono mb-5 text-4xl font-bold">Login</h2>
           <p className="max-w-sm mb-12 font-sans font-light text-gray-600">
-            Log in to your account to upload or download pictures, videos or
-            music
+            Log in to your account to view your highschool account
           </p>
           <form onSubmit={onSubmit}>
             <div className="flex flex-col space-y-3">
               <input
                 ref={emailRef}
-                type="text"
+                type="email"
                 className="w-full p-6 border border-gray-300 rounded-md placeholder:font-sans placeholder:font-light"
                 placeholder="Enter your email address"
               />
