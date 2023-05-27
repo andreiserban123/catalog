757aafbfd7e575e1d55565a3d516d0f66298fb6c
Andrei Serban <andyspeed2003@gmail.com>
2023-05-27 19:08:15 +0300

    refactor

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,14 @@
-const express = require("express");
-require("dotenv").config();
-const session = require("express-session");
-const port = process.env.PORT || 5000;
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.port || 5000;
+import userRoutes from "./routes/userRoutes.js";
+
 const app = express();
-const cors = require("cors");
-const db = require("./config/db");
 
-//Body parser middleware
+app.use("/api/users", userRoutes);
 
-//starting the db
-db.createPool();
+app.get("/", (req, res) => res.send("hello world"));
 
-app.use(express.json());
-
-app.use(
-  session({
-    secret: "mysecret",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5000",
-      "http://127.0.0.1:5000",
-      "http://127.0.0.1:5173",
-    ],
-    credentials: true,
-  })
-);
-
-app.use(express.urlencoded({ extended: false }));
-
-const authRouter = require("./routes/auth");
-app.use("/api/auth", authRouter);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server started on ${port}`));
