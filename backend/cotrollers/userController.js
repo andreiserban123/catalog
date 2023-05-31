e8860b5b94ff9e186e5ee51739a2ef7334d07705
Andrei Serban <andyspeed2003@gmail.com>
2023-05-31 17:58:27 +0300

    ok

--- backend/cotrollers/userController.js
+++ backend/cotrollers/userController.js
@@ -2,13 +2,6 @@ import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
 import oracledb from "oracledb";
 
-// @desc    auth user /set token
-// route    POST /api/users/auth
-// @access  PUBLIC
-const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
-});
-
 // @desc    login user
 // route    POST /api/users/login
 // @access  PUBLIC
@@ -18,11 +11,18 @@ const loginUser = asyncHandler(async (req, res) => {
   const query = `SELECT * FROM SIT_USER WHERE EMAIL = :email AND PASSWORD = SIT_UTILS_PKG.ENCRYPT_PASSWORD(:password)`;
   const result = await connection.execute(query, { email, password });
   await connection.close();
-  if (result.rows.length === 0) {
+  if (result.rows.length > 0) {
+    const user = result.rows[0];
+    generateToken(res, user[0]);
+    res.status(200).json({
+      id: user[0],
+      name: user[1],
+      email: user[2],
+    });
+  } else {
     res.status(401);
     throw new Error("Invalid email or password");
   }
-  res.status(200).json({ message: "login User" });
 });
 
 // @desc    logout user
@@ -46,4 +46,4 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Update User Profile" });
 });
 
-export { loginUser, authUser, logoutUser, getUserProfile, updateUserProfile };
+export { loginUser, logoutUser, getUserProfile, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,14 +2,12 @@ import express from "express";
 const router = express.Router();
 import {
   loginUser,
-  authUser,
   logoutUser,
   getUserProfile,
   updateUserProfile,
 } from "../cotrollers/userController.js";
 
 router.post("/", loginUser);
-router.post("/auth", authUser);
 router.post("/logout", logoutUser);
 router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
