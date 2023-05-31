d5520fad23a8955d436aeb37ebbc10763da15f67
Andrei Serban <andyspeed2003@gmail.com>
2023-05-31 22:37:57 +0300

    update JWT

--- backend/cotrollers/userController.js
+++ backend/cotrollers/userController.js
@@ -10,16 +10,21 @@ const loginUser = asyncHandler(async (req, res) => {
   const connection = await oracledb.getConnection();
   const query = `SELECT * FROM SIT_USER WHERE EMAIL = :email AND PASSWORD = SIT_UTILS_PKG.ENCRYPT_PASSWORD(:password)`;
   const result = await connection.execute(query, { email, password });
-  await connection.close();
   if (result.rows.length > 0) {
     const user = result.rows[0];
-    generateToken(res, user[0]);
+    // console.log(user[0]); // id
+    const userID = user[0];
+    const query = `SELECT ID FROM SIT_USER_ROLES WHERE ID_user = :userID`;
+    const result2 = await connection.execute(query, { userID });
+    const id_user_roles = result2.rows[0][0];
+    generateToken(res, id_user_roles);
     res.status(200).json({
-      id: user[0],
-      name: user[1],
+      id: id_user_roles,
       email: user[2],
     });
+    await connection.close();
   } else {
+    await connection.close();
     res.status(401);
     throw new Error("Invalid email or password");
   }
@@ -29,7 +34,12 @@ const loginUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/logout
 // @access  PUBLIC
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "LogoutUser User" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User Logout with success" });
 });
 
 // @desc    get profile user
